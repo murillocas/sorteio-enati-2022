38dc39bd6037bf2b608be038c71fe3511b22420a
murillocas <murillocastro2009@hotmail.com>
2022-06-01 09:11:19 -0300

    função sortear

--- sorteio.js
+++ sorteio.js
@@ -1,22 +1,26 @@
-fetch("teste2222.json")
-.then(response => {
-   return response.json();
-})
-.then(jsondata => {
-    let aux = 0
-    let test 
-    let terminar = -10
-    for(aux;terminar < 0;aux++){
-        test = jsondata[aux]
-        if(test == null){
-            terminar = +10
-        }else{
-            aux += 1
-        }
-    }
-    console.log('teste')
-    console.log(Math.floor(Math.random() * aux))
-    console.log(aux-1)
-    let auxjson = 
-    console.log(jsondata[Math.floor(Math.random() * aux)].Nome)
-})
+function sortear() {
+    fetch("teste2222.json")
+        .then(response => {
+            return response.json();
+        })
+        .then(jsondata => {
+            let aux = 0
+            let test
+            let terminar = -10
+            for (aux; terminar < 0; aux++) {
+                test = jsondata[aux]
+                if (test == null) {
+                    terminar = +10
+                } else {
+                    aux += 1
+                }
+            }
+            console.log('teste')
+            console.log(Math.floor(Math.random() * aux))
+            console.log(aux - 1)
+            let auxjson =
+                console.log(jsondata[Math.floor(Math.random() * aux)].Nome)
+        })
+}
+
+sortear();
